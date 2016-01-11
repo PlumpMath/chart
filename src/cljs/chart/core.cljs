c4ed1890a04b964831c8addcca0ea97b9d99d2f9
philos <philos99@gmail.com>
2016-01-11 21:40:21 +0900

    단순 막대 그래프 갱신

--- src/cljs/chart/core.cljs
+++ src/cljs/chart/core.cljs
@@ -1,6 +1,7 @@
-;; (ns chart.core
-;;   (:require [reagent.core :as r]
-;;             [cljsjs.d3] ))
+(ns chart.core
+  (:require [reagent.core :as r]
+            [cljsjs.d3]
+            [cljs.debux :refer-macros [dbg clog break]] ))
 
 ;; (defn simple-component []
 ;;   [:div
@@ -20,33 +21,29 @@
 ;;     (select "#someDiv")
 ;;     (style "background" "yellow"))
 
-(ns chart.core
-  (:require [reagent.core :as r]
-            [cljsjs.d3]
-            [cljs.debux :refer-macros [dbg clog break]] ))
-
 (def data* (r/atom [10 15 30 50 80 65 55 30 20 10 8]))
 
-(defn init-bar [this]
-  (let [data (.. js/d3
-                 (select "body")
-                 (selectAll "div.h-bar")
-                 (data (clj->js @data*)))]
+(defn render-bar [this data]
+  (let [data' (.. js/d3
+                  (select "#bar")
+                  (append "div.simple-bar")
+                  (selectAll "div.h-bar")
+                  (data (clj->js data)))]
     ;; enter
-    (.. data
+    (.. data'
         enter
         (append "div")
         (attr "class" "h-bar")
         (append "span"))
 
     ;; update
-    (.. data
+    (.. data'
         (style "width" #(str (* 3 %) "px"))
         (select "span")
         (text #(identity %)))
                 
      ;; exit
-     (.. data
+     (.. data'
          exit
          remove) ))
   
@@ -59,20 +56,18 @@
          (draw-d3-gauge @dom-node args ...)))
 
      :component-did-mount
-     init-bar
+     #(render-bar % @data*)
 
      :reagent-render
      (fn [this]
        ;; Necessary for Reagent to see that we depend on the dom-node r/atom.
        ;; Note: we don't actually use any of the args here.  This is because
        ;; we cannot render D3 at this point.  We have to wait for the update.
-       @data*
-       [:div "Simple bar"])}))
+       
+       [:div "Simple bar" (render-bar this @data*)])}))
 
-(r/render [simple-bar] (.getElementById js/document "app"))
+
+(r/render [simple-bar] (.getElementById js/document "bar"))
 
 (clog "hello")
-
-
-
-
+(reset! data* [10 20 30 40 50 60 70 80 90 100 110])
--- src/cljs/chart/posh.clj
+++
@@ -1,6 +0,0 @@
- (ns chart.posh
-   (:require [reagent.core :as r]
-             [datascript.core :as d]
-             [posh.core :refer [pull q db-tx pull-tx q-tx after-tx! before-tx!
-                                transact! posh!] ]))
- 
