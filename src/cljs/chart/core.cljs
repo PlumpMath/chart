046ed52e038ce19879a06a317ae90c452845275b
philos <philos99@gmail.com>
2016-01-12 11:23:21 +0900

    simple-bar refactored.

--- src/cljs/chart/core.cljs
+++ src/cljs/chart/core.cljs
@@ -21,12 +21,12 @@
 ;;     (select "#someDiv")
 ;;     (style "background" "yellow"))
 
-(def data* (r/atom [10 15 30 50 80 65 55 30 20 10 8]))
+(def data* (r/atom [10 20 30 40 50 60 70 80 90 100]))
 
-(defn render-bar [this data]
+(defn render-bar [data]
+  (clog [data])
   (let [data' (.. js/d3
                   (select "#bar")
-                  (append "div.simple-bar")
                   (selectAll "div.h-bar")
                   (data (clj->js data)))]
     ;; enter
@@ -49,25 +49,16 @@
   
 (defn simple-bar []
   (r/create-class
-    {#_:component-did-update
-     #_(fn [this old-argv]
-       (let [[_ args ...] (r/argv this)]
-         ;; This is where we get to actually draw the D3 gauge.
-         (draw-d3-gauge @dom-node args ...)))
-
-     :component-did-mount
-     #(render-bar % @data*)
-
-     :reagent-render
+    {:reagent-render
      (fn [this]
-       ;; Necessary for Reagent to see that we depend on the dom-node r/atom.
-       ;; Note: we don't actually use any of the args here.  This is because
-       ;; we cannot render D3 at this point.  We have to wait for the update.
-       
-       [:div "Simple bar" (render-bar this @data*)])}))
+       [:div "Simple bar" (render-bar @data*)])}))
 
 
 (r/render [simple-bar] (.getElementById js/document "bar"))
 
-(clog "hello")
-(reset! data* [10 20 30 40 50 60 70 80 90 100 110])
+(defn shift [data]
+  (conj (vec (drop 1 data)) 110))
+
+(swap! data* shift)
+
+
